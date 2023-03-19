6a59221ff6c58ad2b1ab0ede998fa605603c12fc
kerain996 <42693540+kerain996@users.noreply.github.com>
2023-03-19 10:44:48 +0800

    Add files via upload

---
+++ Game/Destiny2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using SeasonPassStalker;
+using System.Text.Encodings.Web;
+using System.Text.RegularExpressions;
+
+namespace PassStalker.Game
+{
+    internal class Destiny2 : SeasonPassStalker.SeasonPass
+    {
+
+        public Destiny2(SeasonPassStalker.Game gameInfo) : base(gameInfo)
+        {
+            SeasonPassStalker.Game d2 = new SeasonPassStalker.Game();
+            d2.setValues(1, "Destiny2", "Destiny2 is a ...", "", "Bungie", "PC/Epic");
+            game = d2;
+        }
+
+        public void getPassInfo()
+        {
+            // 信息处理
+            
+        }
+    }
+}
---
+++ Game/YugiohMD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeasonPassStalker;
+using System.Net.Http;
+
+namespace PassStalker.Game
+{
+    internal class YugiohMD : SeasonPass
+    {
+        public YugiohMD(SeasonPassStalker.Game gameinfo) : base(gameinfo)
+        {
+
+        }
+    }
+}
